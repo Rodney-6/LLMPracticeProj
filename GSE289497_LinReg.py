519130db3157bac6c70ec9f6f761f6919c7d0991
rodney-6 <rodneyshafer@gmail.com>
2025-05-30 13:37:43 -0600

    I'm going outside

--- GSE289497_LinReg.py
+++ GSE289497_LinReg.py
@@ -26,6 +26,17 @@ matplotlib.rcParams['font.size'] = 14
 matplotlib.rcParams['figure.figsize'] = (10, 6)
 matplotlib.rcParams['figure.facecolor'] ="#B9B1B100"
 
+#generically plot distributions
+# for i in medical_df:
+#         fig = px.histogram(medical_df,
+#                            x=i,
+#                            marginal='box',
+#                            title=f'Distribution of {i}')
+#         fig.update_layout(bargap=0.1)
+#         fig.show()
+        
+
+# Plot age data
 fig = px.histogram(medical_df, 
                    x='age', 
                    marginal='box', 
@@ -33,3 +44,26 @@ fig = px.histogram(medical_df,
                    title='Distribution of Age')
 fig.update_layout(bargap=0.1)
 fig.show()
+
+# plot BMI data
+fig = px.histogram(medical_df, 
+                   x='bmi',
+                   marginal='box',
+                   color_discrete_sequence=['red'],
+                   title='Distribution of BMI (Body Mass Index)')
+fig.update_layout(bargap=0.1)
+fig.show()
+
+# plot the charges to evaluate if there are any correltaions
+fig = px.histogram(medical_df,
+                   x='charges',
+                   marginal= 'box',
+                   color='smoker',
+                   color_discrete_sequence=['green', 'grey'],
+                   title='Annual Medical Charges')
+fig.update_layout(bargap=0.1)
+fig.show()
+
+#We are itnerested in charges and there seems to be a correlation
+#let's loook at smoker data:
+medical_df.smoker.value_counts()
