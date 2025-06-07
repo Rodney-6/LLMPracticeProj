f69a8fc14bcfaf673ab85a27607848ebf069f0f9
Rodney Shafer <the_rodney@LAPTOP-MEN9DE81.>
2025-06-07 17:57:03 -0600

    moving on to class two

--- Lin_Log_regression/GSE289497_LinReg.py
+++ Lin_Log_regression/GSE289497_LinReg.py
@@ -92,3 +92,21 @@ fig = px.scatter(medical_df,
 fig.update_traces(marker_size=5)
 fig.show()
 
+
+years = range(2000, 2012)
+apples = [0.895, 0.91, 0.919, 0.926, 0.929, 0.931, 0.934, 0.936, 0.937, 0.9375, 0.9372, 0.939]
+oranges = [0.962, 0.941, 0.930, 0.923, 0.918, 0.908, 0.907, 0.904, 0.901, 0.898, 0.9, 0.896, ]
+plt.plot(years, apples)
+plt.plot(years, oranges)
+plt.xlabel('Year')
+plt.ylabel('Yield (tons per hectare)');
+
+cherries = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1]
+plt.plot(years, apples)
+plt.plot(years, cherries)
+
+plt.xlabel('Year')
+plt.ylabel('Yield (tons per hectare)')
+
+plt.title("Crop Yields in Kanto")
+plt.legend(['Apples', 'Cherries']);
